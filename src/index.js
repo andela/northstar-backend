67aa15ca219f3cb7b32cd08545e5c5da55d0e41a
chiomadans1759 <chiomadans@gmail.com>
2019-08-14 22:57:10 +0100

    167906424 ch-Setup Babel and Nodemon

--- src/index.js
+++ src/index.js
@@ -38,23 +38,23 @@ if (!isProduction) {
     app.use(errorhandler());
 }
 
-if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
-}
+// if (isProduction) {
+//     mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//     mongoose.connect("mongodb://localhost/conduit");
+//     mongoose.set("debug", true);
+// }
 
 require("./models/User");
 
 app.use(require("./routes"));
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     const err = new Error("Not Found");
+//     err.status = 404;
+//     next(err);
+// });
 
 /// error handlers
 
@@ -85,6 +85,16 @@ app.use(function(err, req, res, next) {
             error: {}
         }
     });
+});
+
+//App index
+app.get('/', (req, res) => {
+	res.json({ Message: 'Welcome! This is the Northstar Barefood Nomad home page.' });
+});
+
+// catch 404 and forward to error handler
+app.get('*', (req, res) => {
+	res.json({ Message: 'Endpoint Not Found' });
 });
 
 // finally, let's start our server...
