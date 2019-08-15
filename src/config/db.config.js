33a7c6ca676332b44f489d14b7dc6856bea2c682
Osahon Oboite <osahonoboite@gmail.com>
2019-08-15 05:47:06 +0100

    connect postgresql to sequelize

---
+++ src/config/db.config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    development: {
+        username: process.env.DEV_DB_USER,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DATABASE,
+        host: '127.0.0.1',
+        dialect: 'postgres'
+    },
+    test: {
+        username: process.env.TEST_DB_USER,
+        password: process.env.TEST_DB_PASSWORD,
+        database: process.env.TEST_DATABASE,
+        host: '127.0.0.1',
+        dialect: 'postgres'
+    },
+    production: {
+        use_env_variable: process.env.DATABASE_URL,
+        dialect: 'postgres'
+    }
+};
