b7aaba35bbd2c6124b52de9841cd0a69664ddf17
Osahon Oboite <osahonoboite@gmail.com>
2019-08-16 07:38:21 +0100

    change default es5 to es6 syntax

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/db.config.js')[env];
