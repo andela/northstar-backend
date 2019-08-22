32f58b777984cb627ac4177d9f21e7b1eff539d6
chiomadans1759 <chiomadans@gmail.com>
2019-08-22 21:23:55 +0100

    #167727451 Completed the user signup

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -32,4 +32,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,1 +1,6 @@
+import Users from '../../controllers/users';
 
+export default (server) => {
+  /* Users Routes Here */
+  server.post('/api/v1/auth/signup', Users.signup);
+};
