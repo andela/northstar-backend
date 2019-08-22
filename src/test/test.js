e5175b2c0818ee590e25dd558ab48f96e17ce46e
chiomadans1759 <chiomadans@gmail.com>
2019-08-22 21:23:55 +0100

    ft(User Signup):Created the user signup endpoint

--- src/test/test.js
+++ src/test/test.js
@@ -1,16 +1,41 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-
-const { expect } = chai;
+import app from '../index';
 
 chai.use(chaiHttp);
+chai.should();
 
-// A simple test case to test proper setup
-describe('Basic Mocha String Test', () => {
-  it('should return number of charachters in a string', () => {
-    expect(2).to.equal(2);
-  });
-  it('should return first charachter of the string', () => {
-    expect(2).to.not.equal(1);
+const user = {
+  email: 'chioma@godtryere.com',
+  first_name: 'Ejike',
+  last_name: 'Chiemerie',
+  password: 'secret123',
+  role: 'requester',
+  gender: 'female',
+  birth_date: '02-02-2019',
+  preferred_language: 'english',
+  preferred_currency: 'USD',
+  location: 'lagos',
+  isVerified: false
+};
+
+describe('Users', () => {
+  // Test for creating new user
+  describe('/POST register users', () => {
+    it('it should Signup a user and generate a token', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.an('object');
+          res.body.should.have.property('status').eql('success');
+          res.body.should.have.property('data');
+          res.body.data.should.have.property('first_name');
+          res.body.data.should.have.property('last_name');
+          res.body.data.should.have.property('email');
+          done();
+        });
+    });
   });
 });
